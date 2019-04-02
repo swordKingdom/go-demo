c811b591e5a1031b66e7e3039b4fb860512b3bf7
kajun <2587332053@qq.com>
2019-04-02 22:28:15 +0800

    增加设计模式代码

---
+++ design_pattern/generator/generator.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+//生成器模式
+func Count(start int, end int) chan int {
+	ch := make(chan int)
+
+	go func(ch chan int) {
+		for i := start; i <= end ; i++ {
+			// Blocks on the operation
+			ch <- i
+		}
+
+		close(ch)
+	}(ch)
+
+	return ch
+}
+
+func main()  {
+	for i := range Count(1, 99) {
+		fmt.Println("Pass it around, put one up,", i, "bottles of beer on the wall")
+	}
+}
