0bc3f780defda93ebe86f5a8fc8eece7f2a95dfb
yangjiajun <yangjiajun@kingsoft.com>
2019-09-05 15:20:16 +0800

    数据结构：双向链表，树，以及队列

---
+++ datastruct/linkedlist/bidirectional_linked_list.go
@@ -0,0 +1,43 @@
+package linkedlist
+
+import "sync"
+
+type BNode struct {
+	Val  interface{}
+	Pre  *BNode
+	Next *BNode
+}
+
+type BLinkedListHead struct {
+	head *BNode
+	Len  int
+	lock *sync.RWMutex
+}
+
+func (b *BLinkedListHead) InsertNode(val interface{}, index int) {
+
+}
+
+func (b *BLinkedListHead) DeleteNode(index int) {
+	panic("implement me")
+}
+
+func (b *BLinkedListHead) GetNode(index int) *Node {
+	panic("implement me")
+}
+
+func InitBLinkedList(arr []interface{}) *BLinkedListHead {
+	arrLen := len(arr)
+	if arrLen == 0 {
+		return nil
+	}
+	head := &BNode{Val: arr[0]}
+	res := &BLinkedListHead{head: head, Len: arrLen, lock: new(sync.RWMutex)}
+	for i := 1; i < arrLen; i++ {
+		node := &BNode{Val: arr[i]}
+		head.Next = node
+		head.Next.Pre = node
+		head = head.Next
+	}
+	return res
+}
