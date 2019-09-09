15d884d63014e1f040c38b756d552947cce2477f
yangjiajun <yangjiajun@kingsoft.com>
2019-09-09 11:20:23 +0800

    数据结构：平衡二叉树

--- datastruct/tree/avltree.go
+++ datastruct/tree/avltree.go
@@ -5,12 +5,14 @@ type AvlTreeNode struct {
 	Val    interface{}
 	Left   *AvlTreeNode
 	Right  *AvlTreeNode
+	b      int8
 }
 
 type AvlTree struct {
-	root *AvlTreeNode
-	size int
-	m    int
+	root       *AvlTreeNode
+	size       int
+	m          int
+	comparator func(a, b interface{}) int8
 }
 
 func (a *AvlTree) IsEmpty() bool {
@@ -25,4 +27,58 @@ func (a *AvlTree) Clear() {
 
 func (a *AvlTree) Size() int {
 	return a.size
+}
+
+func putFix(c int8, node **AvlTreeNode) bool {
+	s := *node
+	if s.b == 0 {
+		s.b = c
+		return true
+	}
+
+	if s.b == -c {
+		s.b = 0
+		return false
+	}
+	var b int8
+	if (c+1)/2 == 0 {
+		b = s.Left.b
+	} else {
+		b = s.Right.b
+	}
+	if b == c {
+		//单旋
+
+	} else {
+		//双旋
+	}
+	*node = s
+	return false
+}
+
+func (a *AvlTree) put(val interface{}, parent *AvlTreeNode, children **AvlTreeNode) bool {
+	q := *children
+	if q == nil {
+		a.size++
+		*children = &AvlTreeNode{Val: val, Parent: parent}
+		return true
+	}
+	cmp := a.comparator(val, q.Val)
+	if cmp == 0 {
+		return false
+	}
+	var fix bool
+	if cmp > 0 {
+		fix = a.put(val, q, &q.Right)
+	} else {
+		fix = a.put(val, q, &q.Left)
+	}
+	if fix {
+		return putFix(cmp, children)
+	}
+	return false
+}
+
+func (a *AvlTree) Put(val int) {
+
 }
